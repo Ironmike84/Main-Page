2c6f49773efa777e1e5ced71d22d3b8f3aa1ee75
Mike <87212248+Ironmike84@users.noreply.github.com>
2021-12-14 13:19:54 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
-let sendButton = document.getElementById("send");
 
-sendButton.addEventListener("click", function(){
-let form = document.getElementById("contactForm")    
-let comments = document.getElementById("comments").value
-    let eMail = JSON.stringify(comments)
-    .form
-    postMessage = eMail 
-
-console.log(comments)
-
-
-
-
-function checkInput(e){
-    
-inputs = document.querySelectorAll('input').value
-
-if (inputs !== null){
-console.log("Empty")
-console.log(e)
-}else if(inputs === null){
-    console.log("full")
-    console.log(e)
-}else{
-console.log("failed")
-}
-}
-checkInput()
-
+let cardImageTwo = document.getElementById('cardImageTwo')
+let cardTwoDesc = document.getElementById('cardTwoDesc')
+cardTwoDesc.addEventListener('mouseover', function(){
+   cardImageTwo.style.opacity = '.1'
+   cardImageTwo.style.transition = '2s ease'
+   
+})
+cardTwoDesc.addEventListener('mouseleave', function(){
+   cardImageTwo.style.opacity = '1'
+   cardImageTwo.style.transition = '2s ease'
+   
 })
 
+let cardImageOne = document.getElementById('cardImageOne')
+let cardOneDesc = document.getElementById('cardOneDesc')
+cardOneDesc.addEventListener('mouseover', function(){
+   cardImageOne.style.opacity = '.1'
+   cardImageOne.style.transition = '2s ease'
+   
+})
+cardOneDesc.addEventListener('mouseleave', function(){
+   cardImageOne.style.opacity = '1'
+   cardImageOne.style.transition = '2s ease'
+   
+})
 
-
-
+let cardImageThree = document.getElementById('cardImageThree')
+let cardThreeDesc = document.getElementById('cardThreeDesc')
+cardThreeDesc.addEventListener('mouseover', function(){
+   cardImageThree.style.opacity = '.1'
+   cardImageThree.style.transition = '2s ease'
+   
+})
+cardThreeDesc.addEventListener('mouseleave', function(){
+   cardImageThree.style.opacity = '1'
+   cardImageThree.style.transition = '2s ease'
+   
+})
