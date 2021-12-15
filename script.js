ed68402f0ca3077bdd5d987253a59ddde34773fc
Mike <87212248+Ironmike84@users.noreply.github.com>
2021-12-15 17:35:58 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 
-let cardImageTwo = document.getElementById('cardImageTwo')
-let cardTwoDesc = document.getElementById('cardTwoDesc')
-cardTwoDesc.addEventListener('mouseover', function(){
-   cardImageTwo.style.opacity = '.4'
-   cardImageTwo.style.transition = '2s ease'
-   
-})
-cardTwoDesc.addEventListener('mouseleave', function(){
-   cardImageTwo.style.opacity = '1'
-   cardImageTwo.style.transition = '2s ease'
-   
-})
-
 let cardImageOne = document.getElementById('cardImageOne')
-let cardOneDesc = document.getElementById('cardOneDesc')
-cardOneDesc.addEventListener('mouseover', function(){
-   cardImageOne.style.opacity = '.4'
+let cardUnoDesc = document.getElementById('cardUnoDesc')
+cardUnoDesc.addEventListener('mouseover', function(){
+   cardImageOne.style.opacity = '.5'
    cardImageOne.style.transition = '2s ease'
    
 })
-cardOneDesc.addEventListener('mouseleave', function(){
+cardUnoDesc.addEventListener('mouseleave', function(){
    cardImageOne.style.opacity = '1'
    cardImageOne.style.transition = '2s ease'
    
 })
 
+
+let cardImageTwo = document.getElementById('cardImageTwo')
+let cardDoceDesc = document.getElementById('cardDoceDesc')
+cardDoceDesc.addEventListener('mouseover', function(){
+   cardImageTwo.style.opacity = '.5'
+   cardImageTwo.style.transition = '2s ease'
+   
+})
+cardDoceDesc.addEventListener('mouseleave', function(){
+   cardImageTwo.style.opacity = '1'
+   cardImageTwo.style.transition = '2s ease'
+   
+})
+
+
 let cardImageThree = document.getElementById('cardImageThree')
-let cardThreeDesc = document.getElementById('cardThreeDesc')
-cardThreeDesc.addEventListener('mouseover', function(){
-   cardImageThree.style.opacity = '.4'
+let cardTraceDesc = document.getElementById('cardTraceDesc')
+cardTraceDesc.addEventListener('mouseover', function(){
+   cardImageThree.style.opacity = '.5'
    cardImageThree.style.transition = '2s ease'
    
 })
-cardThreeDesc.addEventListener('mouseleave', function(){
+cardTraceDesc.addEventListener('mouseleave', function(){
    cardImageThree.style.opacity = '1'
    cardImageThree.style.transition = '2s ease'
    
