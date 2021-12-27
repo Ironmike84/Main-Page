0a496bf3e4cad138549d993d3578ebd14ccff733
Mike <87212248+Ironmike84@users.noreply.github.com>
2021-12-27 10:01:08 -0500

    Update
    New Update

--- script.js
+++ script.js
@@ -4,12 +4,11 @@ let cardUnoDesc = document.getElementById('cardUnoDesc')
 cardUnoDesc.addEventListener('mouseover', function(){
    cardImageOne.style.opacity = '.3'
    cardImageOne.style.transition = '2s ease'
-   
 })
 cardUnoDesc.addEventListener('mouseleave', function(){
    cardImageOne.style.opacity = '1'
    cardImageOne.style.transition = '2s ease'
-   
+
 })
 
 
@@ -18,12 +17,11 @@ let cardDoceDesc = document.getElementById('cardDoceDesc')
 cardDoceDesc.addEventListener('mouseover', function(){
    cardImageTwo.style.opacity = '.3'
    cardImageTwo.style.transition = '2s ease'
-   
 })
 cardDoceDesc.addEventListener('mouseleave', function(){
    cardImageTwo.style.opacity = '1'
    cardImageTwo.style.transition = '2s ease'
-   
+
 })
 
 
@@ -32,12 +30,11 @@ let cardTraceDesc = document.getElementById('cardTraceDesc')
 cardTraceDesc.addEventListener('mouseover', function(){
    cardImageThree.style.opacity = '.3'
    cardImageThree.style.transition = '2s ease'
-   
 })
 cardTraceDesc.addEventListener('mouseleave', function(){
    cardImageThree.style.opacity = '1'
    cardImageThree.style.transition = '2s ease'
-   
+
 })
 
 
@@ -46,10 +43,16 @@ let cardQuattroDesc = document.getElementById('cardQuattroDesc')
 cardQuattroDesc.addEventListener('mouseover', function(){
    cardImageFour.style.opacity = '.3'
    cardImageFour.style.transition = '2s ease'
-   
 })
 cardQuattroDesc.addEventListener('mouseleave', function(){
    cardImageFour.style.opacity = '1'
    cardImageFour.style.transition = '2s ease'
-   
 })
+
+let summaryContainer = document.getElementById('summaryContainer.')
+let html = document.getElementById('htmlHide');
+let css = document.getElementById('cssHide');
+let js = document.getElementById('jsHide');
+
+
+
