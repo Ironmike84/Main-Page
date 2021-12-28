fd815272aab2f4ca7ee8e43d250af07f4e76ba85
Mike <87212248+Ironmike84@users.noreply.github.com>
2021-12-28 15:12:54 -0500

    Update

--- script.js
+++ script.js
@@ -61,28 +61,28 @@ bar1Button.addEventListener("click", function(){
 appendHousing()
 })
 
-foodButton.addEventListener("click", function(){
+ foodButton.addEventListener("click", function(){
     appendFood()
-})
+ })
 
-utilitiesButton.addEventListener("click", function(){
-    appendUtil()
-})
+ utilitiesButton.addEventListener("click", function(){
+     appendUtil()
+ })
 
-carButton.addEventListener("click", function(){
+ carButton.addEventListener("click", function(){
     appendCar()
 })
 
 totalExpenses.addEventListener("click", function(){
         getTotal()
-})
+ })
 
 //--------------------------------------------------------------------------------------------BAR APPEND FINANCE DETAILS
 
 function appendHousing(){
     let total= parseInt(document.getElementById("total").value);
     let housing = parseInt(document.getElementById("housing").value);
-    let housingCost =  total - housing
+    let housingCost =  total - housing 
     let percentage = Math.floor((housing / total) * 100)
     let box1 = document.getElementById("box1");
 
@@ -94,23 +94,26 @@ function appendHousing(){
     budget.push({housing:housingCost})
     totals.push({totalHousing:housing})
     box1.append(` The total housing cost is $${housing}, Which is ${percentage}% of your total income.`)
+ 
+
 }
 
 function appendFood(){
-    let afterHousing = budget[2].housing;
-    let food = document.getElementById("food").value;
-    let total= parseInt(document.getElementById("total").value);
-    let box2 = document.getElementById("box2");
-    let afterFood = afterHousing - food;
-    let percentage = Math.floor((food / total) * 100);
+ let afterHousing = budget[2].housing;
+ let food = document.getElementById("food").value;
+ let total= parseInt(document.getElementById("total").value);
+ let box2 = document.getElementById("box2");
+ let afterFood = afterHousing - food;
+ let percentage = Math.floor((food / total) * 100); 
+    
     bar2.style.width = (`${percentage}%`);
     bar2.append(`${percentage}%`)
     foodTotal.append(`$${afterFood}`)
     console.log(afterFood)
-    console.log(`${percentage}%`)
-    budget.push({food:afterFood})
-    totals.push({foodTotal:food})
-    box2.append(` The total housing cost is $${food}, Which is ${percentage}% of your total income.`)
+ console.log(`${percentage}%`)
+ budget.push({food:afterFood})
+ totals.push({foodTotal:food})
+ box2.append(` The total housing cost is $${food}, Which is ${percentage}% of your total income.`)
 }
 
 function appendUtil(){
@@ -160,12 +163,13 @@ function getTotal(){
     let yearlySaving = document.getElementById("yearlySavings");
 
     let dti = totalExpense / total
+    
     dtiBar.style.width = (`${dti * 10}rem`)
     dtiBar.append(`${parseInt(dti*100)}%`)
     console.log(`DTI:${dti}`);
     console.log(totalExpense);
     totalBar.append(`$${totalExpense} -  ${percentage}%`)
-    totalBar.style.width = (`${totalExpense/100}em`)
+    totalBar.style.width = (`${totalExpense/100}em`)    
     totals.push({totalExpense:totalExpense})
     let yearlyIncome=(total*12)
     let yearlyExpense=(totalExpense*12)
@@ -188,8 +192,10 @@ housingButton.addEventListener('click', function(){
     let housingAdd = document.getElementById('housingAdd');
     housingAdd.classList.toggle('housingHide');
     housingButton.classList.toggle('houseButtonToggle');
+    
     housingAddTitleToggle.classList.toggle('housingAddTitle');
 
+    
 })
 
 //-------------------------------------------------------------------------------------ADD & SEND HOUSING To INPUT
@@ -199,6 +205,7 @@ addHousing.addEventListener('click', function(){
     let propertyTaxes = parseInt(document.getElementById('propertyTaxes').value);
     let mortgageCost = parseInt(document.getElementById('mortgageCost').value);
     let totalHousing = document.querySelector('#housing');
+    
     let total = maintainence + propertyTaxes+ mortgageCost
     totalHousing.value = total
     console.log(totalHousing)
@@ -213,6 +220,7 @@ foodButton2.addEventListener('click', function(){
     foodAdd.classList.toggle('foodHide');
     foodButton2.classList.toggle('foodButtonToggle');
     foodAddTitleToggle.classList.toggle('foodAddTitle');
+    
 })
 
 //-------------------------------------------------------------------------------------ADD & SEND HOUSING To INPUT
@@ -226,6 +234,8 @@ addFood.addEventListener('click', function(){
 
     let foodTotal = grocery + dining + snacks + alchohol
     totalFood.value = foodTotal;
+    
+    
 })
 //-----------------------------------------------------------------------------------------------------------UTILITIES
 //-------------------------------------------Food View TOGGLE BUTTON
@@ -236,6 +246,7 @@ utilButton.addEventListener('click', function(){
     utilAdd.classList.toggle('utilHide');
     utilButton.classList.toggle('utilButtonToggle');
     utilAddTitleToggle.classList.toggle('utilAddTitle');
+    
 })
 
 //-------------------------------------------------------------------------------------ADD & SEND HOUSING To INPUT
@@ -250,6 +261,8 @@ addUtil.addEventListener('click', function(){
 
     let utilTotal = electric + gas + water + cable
     totalUtil.value = utilTotal;
+    
+    
 })
 
 //-----------------------------------------------------------------------------------------------------------CAR
@@ -259,10 +272,10 @@ car2Button.addEventListener('click', function(){
     let carAddTitleToggle = document.getElementById('carAddTitleToggle');
     let carAdd = document.getElementById('carAdd');
     carAdd.classList.toggle('carHide');
-    carAddTitleToggle.classList.toggle('carAddTitle');
     car2Button.classList.toggle('carButtonToggle');
+    carAddTitleToggle.classList.toggle('carAddTitle');
     
-    })
+})
 
 //-------------------------------------------------------------------------------------ADD & SEND HOUSING To INPUT
 let addCar = document.getElementById('addCar');
@@ -275,40 +288,52 @@ addCar.addEventListener('click', function(){
 
     let carTotal = carGas+ maint + tires + payment
     totalCar.value = carTotal
+
+    
 })
+
 //-----------------------------------------------------------------------------------------------BAR EVENTS
+
 bar1.addEventListener("click", function(){
     // let totalHousing = totals[1].totalHousing
     box1.style.display = "block"
     // alert(`Total Housing is: $${totalHousing} `);
 })
+
 bar1.addEventListener("mouseleave", function(){
     box1.style.display = "none"
 })
+
 bar2.addEventListener("click", function(){
     // let totalFood = totals[2].foodTotal;
     box2.style.display = "block"
     // alert(`Total Food is: $${totalFood} `);
 })
+
 bar2.addEventListener("mouseleave", function(){
     box2.style.display = "none"
 })
+
 bar3.addEventListener("click", function(){
     // let totalUtil = totals[3].totalUtilities;
     box3.style.display = "block"
     // alert(`Total Utilities is: $${totalUtil} `);
 })
+
 bar3.addEventListener("mouseleave", function(){
     box3.style.display = "none"
 })
+
 bar4.addEventListener("click", function(){
     let box4 = document.getElementById("box4");
         box4.style.display = "block"
 })
+
 bar4.addEventListener("mouseleave", function(){
     let box4 = document.getElementById("box4");
     box4.style.display = "none"
 })
+
 
 //-----------------------------------------------------------------------------------------------
 
@@ -365,6 +390,7 @@ netButton.addEventListener("click", function(){
     let cash = parseInt(document.getElementById("cash").value);
     let yourNet = document.getElementById("yourNet");
     let summary = document.getElementById("summary");
+   
 
     let debts = credit + mortgage + carDebt + medical 
     let assets = realEstate + property + investments + cash
@@ -376,10 +402,12 @@ netButton.addEventListener("click", function(){
     yourNet.append(` $${net}`)
     if (net < 121760){
 
+        
         summary.append(`Your NetWorth is $${net}, which is ${percentage}% of the American Average. You are worth $${difference} dollars less than the average, and is ${percentDiff}% less than the national Median amount.`)
     }else if (net > 121760){
         summary.append(`Your NetWorth is $${net}, which is ${percentage}% of the American Average. You are worth $${difference} dollars more than the average, and is ${percentDiff}% more than the national Median amount.`)
     }else{
+     
     }
 
 })
@@ -391,11 +419,13 @@ retirmentCalc.addEventListener("click", function(){
     let interestRate = parseInt(document.getElementById("interestRate").value);
     let years = parseInt(document.getElementById("lengthYears").value);
     let frequency = document.querySelector("#frequency").value;
+    
     let annualRate = annualInt / 100
     let interestRates = interestRate /100
+    
 
-    function invest(){
-        let outputDiv = document.getElementById("output");
+  function invest(){
+        let outputDiv = document.getElementById("output");   
 
         let fv = intitialInv * (1 + annualRate/12)*(years/12)
         let cont = contribution * (1+ interestRates/12)*(years/12)-1
@@ -406,8 +436,9 @@ retirmentCalc.addEventListener("click", function(){
         let summaryContent = document.getElementById("summaryContent")
         console.log(percentage)
 
-        let retirementBar = document.getElementById("retirementBar");
+        let retirementBar = document.getElementById("retirementBar");     
         outputDiv.innerText = (`$${output}`)
+        
         retirementBar.style.width = (`${barLength}mm`)
         retirementBar.innerText = `${percentage}%`
         retirementBar.style.textAlign = "center"
@@ -423,9 +454,9 @@ retirmentCalc.addEventListener("click", function(){
         americasRetirement.style.flexDirection = "column"
         americasRetirement.style.justifyContent = "center"
         summaryContent.innerText = `Your total savings is $${output}, which is ${percentage}% of the American Average Savings`
-}
+  }
 
-invest()
+ invest()
 
 })
 
@@ -446,7 +477,10 @@ function showModal(){
     let closeButton = document.getElementById("close");
     let modalContainer = document.querySelector("#modal-container");
     let picture = document.createElement("img");
+    picture.src = 
+    
     modalContainer.id = "is-visible"
+    
     modalContainer.innerHTML = "<h1>Budget & Finance Calculator</h1>"
     let pic = document.createElement("img");
     pic.src = "finance2.jpeg"
@@ -457,27 +491,28 @@ function showModal(){
     modalContainer.append(closeButton);
     closeButton.style.position = "absolute"
     closeButton.style.top = "1px"
-    closeButton.style.right = "2px"
+    closeButton.style.right = "2px"  
 
 //----------------------------------------------------------------------------ClOSE BUTTON
     let closeButtonElement = document.getElementById("close");;
-
-        closeButtonElement.addEventListener("click", function(){
-        let modal = document.getElementById("modal-container")
-        let image = document.createElement("img");
-        modalContainer.id = "modal-container"
+    
+   closeButtonElement.addEventListener("click", function(){
+       let modal = document.getElementById("modal-container")
+       let image = document.createElement("img");
+       image.src = 
+       modalContainer.id = "modal-container"
         closeButtonElement.style.display = "none"
-        modalContainer.innerHTML = "<div></div>"
-})
+        modalContainer.innerHTML = "<div></div>"   
+       })
 }
 
 //----------------------------------------------------------------------------ESCAPE KEY
 window.addEventListener('keydown', (e) =>{
-        let modalContainer = document.getElementById("is-visible")
-        if (e.key === "Escape"){
-        console.log("Working")
-        modalContainer.id = "modal-container"
-        console.log(modalContainer)
+    let modalContainer = document.getElementById("is-visible")
+    if (e.key === "Escape"){
+      console.log("Working")
+      modalContainer.id = "modal-container"
+      console.log(modalContainer)
     }
 })
 
